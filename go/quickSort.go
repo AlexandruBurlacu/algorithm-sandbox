3f97a5378f23a57e43d561ce123608484497aab4
Alex Burlacu <alexburlacu96@gmail.com>
2017-12-28 20:10:54 +0100

    add quick sort example writen in golang

---
+++ go/quickSort.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func filter(list []int, op func(int) bool) []int {
+	var result []int
+	for _, el := range list {
+		if op(el) {
+			result = append(result, el)
+		}
+	}
+	return result
+}
+
+func smth() bool {
+	if 1 == 1 {
+		return true
+	}
+	return false
+}
+
+func quickSort(list []int) []int {
+
+	if len(list) == 0 {
+		return list
+	}
+
+	pivot, tail := list[0], list[1:]
+	return append(append(quickSort(filter(tail, func(el int) bool {
+		if el < pivot {
+			return true
+		}
+		return false
+	})), pivot), quickSort(filter(tail, func(el int) bool {
+		if el >= pivot {
+			return true
+		}
+		return false
+	}))...)
+}
+
+func main() {
+	var randArr []int
+	for index := 0; index < 10; index++ {
+		randArr = append(randArr, rand.Int()%1000)
+	}
+	fmt.Println(randArr)
+	fmt.Println(quickSort(randArr))
+}
