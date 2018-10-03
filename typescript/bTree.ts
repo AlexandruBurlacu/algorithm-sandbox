ebde7280dceeeeb1f130367432b131ba468f396d
Alex Burlacu <alexburlacu96@gmail.com>
2018-10-04 01:00:57 +0300

    add DFS, needs fix, getting undefined values

--- typescript/bTree.ts
+++ typescript/bTree.ts
@@ -13,16 +13,43 @@ class BTree<T> {
     adjacentNodes: Array<BTree<T>>
     constructor(value: T, ...nodes: Array<BTree<T>>) {
         this.value = value
-        this.adjacentNodes = nodes
+        this.adjacentNodes = nodes || []
     }
+
     toString() {
         return `BTree(${this.value}, ${this.adjacentNodes.map(n => n.toString())})`
     }
+
+    hasValueDFS(value: T) {
+        const dfs = (value: T, node: BTree<T>) => {            
+            const res = node.adjacentNodes.map(x => {
+                console.log(x.adjacentNodes)
+                if(x.adjacentNodes === []) return false
+
+                if (x.value == value) {
+                    return true
+                } else {
+                    return dfs(value, x)
+                }
+            })
+
+            console.log(res) // why the fuck does it get undefined values in a fuckin list??
+
+            res
+        }
+
+        return dfs(value, this)
+    }
 }
 
-console.log(new BTree(1,
-                new BTree(2, new BTree(5)),
-                new BTree(3,
-                    new BTree(6),
-                    new BTree(7)),
-                new BTree(4)))
+let tree = new BTree(1,
+          new BTree(2, new BTree(5)),
+          new BTree(3,
+              new BTree(6),
+              new BTree(7)),
+          new BTree(4))
+
+console.log(tree)
+
+// console.log(tree.hasValueDFS(5))
+
